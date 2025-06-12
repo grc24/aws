9466a7bcc9e778e057609c170041fc6dfc8b213b
SEIDOU <david.seidou@iviplay.com>
2025-06-12 13:44:31 +0100

    remove profile

--- envs/dev/provider.tf
+++ envs/dev/provider.tf
@@ -1,4 +1,3 @@
 provider "aws" {
   region  = "eu-west-3"
-  profile = "761018888105_AdministratorAccess"
 }
--- envs/dev/terraform.tf
+++ envs/dev/terraform.tf
@@ -4,7 +4,6 @@ terraform {
     bucket       = "aws-david-20-dev-s3-bucket-state"
     key          = "terraform.tfstate"
     region       = "eu-west-3"
-    profile      = "761018888105_AdministratorAccess"
     encrypt      = true
     use_lockfile = true
   }
