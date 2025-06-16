6cab627616668170834592625c659bc87c6fb452
SEIDOU <david.seidou@iviplay.com>
2025-06-16 12:34:31 +0100

    add role

--- modules/idp-ci/main.tf
+++ modules/idp-ci/main.tf
@@ -1,6 +1,11 @@
 # Create an IAM OIDC identity provider for token.actions.githubusercontent.com
 resource "aws_iam_openid_connect_provider" "main" {
   url = var.openid_connect_idp_url
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
   client_id_list = [
     var.openid_connect_client_id,
   ]
@@ -31,7 +36,7 @@ resource "aws_iam_role" "main" {
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
-          StringEquals = {
+          StringLike = {
             "token.actions.githubusercontent.com:aud" = "${var.openid_connect_client_id}",
             "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:ref:refs/heads/${var.github_branch}"
           }
@@ -51,13 +56,16 @@ resource "aws_iam_policy" "main" {
   name        = var.epolicy_nam
   description = "Policy for GitHub Actions to access S3 backend"
 
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
+    
       {
         Effect = "Allow",
         Action = [
           "s3:ListBucket",
+          "s3:GetBucketPolicy"
         ],
         Resource = [
           "arn:aws:s3:::${var.s3_bucket_name}"
@@ -83,8 +91,40 @@ resource "aws_iam_policy" "main" {
   }
 }
 
+
+
 # Attach the S3 policy to the GitHub Actions role
 resource "aws_iam_role_policy_attachment" "github_actions_s3_policy_attachment" {
   role       = aws_iam_role.main.name
   policy_arn = aws_iam_policy.main.arn
+}
+
+
+#AllowOIDCProviderAccess
+resource "aws_iam_policy" "allow_oidc_provider_access" {
+  name = "AllowOIDCProviderAccess"
+  description = "Policy for GitHub Actions to access the OIDC provider"
+  tags = {
+    Name        = "Allow OIDC Provider Access"
+    Environment = var.Environment
+  }
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:GetOpenIDConnectProvider"
+        ],
+        Resource = "${aws_iam_openid_connect_provider.main.arn}"
+      },
+    ]
+  })
+}
+
+# Attach the role to the OIDC provider
+resource "aws_iam_role_policy_attachment" "github_actions_oidc_provider_attachment" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.allow_oidc_provider_access.arn
 }
